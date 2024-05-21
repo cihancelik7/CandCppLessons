cf52d97510bdc76588fc23f67843f2c68a86e12a
cihancelik7 <cihan.celik7@gmail.com>
2024-05-21 12:54:08 +0300

    C dilinde char ve string tanimlamasi

--- DegiskenlerC.cpp
+++ DegiskenlerC.cpp
@@ -11,8 +11,22 @@ void degiskenlerC() {
     // sayisallarda %d ile int degerini yazdirabiliriz
     // c++ da ise cout bu durumu kaldiriyor
     printf("Merhaba\n");
-    printf("`*********************Degiskenler C ile tanimlandi*******************************");
+    printf("`*********************Integer veriler C ile tanimlandi*******************************\n");
     // %d = tam sayi yazdiracagim demek, decimal
+    printf("`*********************Char ile veri ve veri dizisi C ile tanimlandi*******************************\n");
+
+    char k = 66;
+    printf("%c \n",k);
+    char k2 = 'C';
+    printf("%d\n",k2);// %d ile belirtirsek bize 67 doner eger %c ile ifade edersek bize C doner
+
+    char k3 = 'Cihan Celik';
+    printf("%c\n",k3);// sadece k yazdi
+    printf("%d\n",k3); // burada ise 107 sonucunu verdi
+
+    char k4[20] = "Cihan Celik";// burada char dizisi olusturacagimizdan bahsettigimiz icin maximum
+    // kelime sayisini dizi sayisi olarak gireriz boylece yazdirir
+    printf("%s",k4);// aslinda tanimlarken string tanimlama yok fakat yazdirirken var
 }
 
 
